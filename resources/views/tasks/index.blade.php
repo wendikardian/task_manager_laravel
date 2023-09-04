fbdc01dc44710d24cb37cf6a662f9dd364e9f026
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-09-04 08:33:45 +0700

    update sidebar

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,22 +8,21 @@ Tasks
 
 <!-- content -->
 @section('content')
-<div class="container">
-    <div class="task-container">
-        <h2>Tasks</h2>
 
-        <div class="task-list">
-            @foreach($tasks as $task)
-            <div class="task-item">
-                <a href="/tasks/{{$task->id}}">
-                    {{$task->title}}
-                </a>
-            </div>
-            @endforeach
+<div class="task-container">
+    <h2>Tasks</h2>
+    <div class="task-list">
+        @foreach($tasks as $task)
+        <div class="task-item">
+            <a href="/tasks/{{$task->id}}">
+                {{$task->title}}
+            </a>
         </div>
-
+        @endforeach
     </div>
+
 </div>
+
 @endsection
 
 <!-- pagination -->
